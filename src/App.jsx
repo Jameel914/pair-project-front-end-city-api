e612c87fd82bf8bf6e2c1c780fc17c4648db39e5
jameel914 <jameelibrahim107@gmail.com>
2023-11-07 20:25:59 -0500

    components created and router setup is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-// import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import IndexPage from "./Components/IndexPage";
+import ShowPage from "./Components/ShowPage";
+import EditPage from "./Components/EditPage";
+import NewPage from "./Components/NewPage";
+import PageNotFound from "./Components/PageNotFound";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities" element={<IndexPage />} />
+          <Route path="/cities/:id" element={<ShowPage />} />
+          <Route path="/cities/:id/edit" element={<EditPage />} />
+          <Route path="/cities/new" element={<NewPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
