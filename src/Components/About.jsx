384c9cb5cc82d12ef050e8a7bae3350a86f66269
jameel914 <jameelibrahim107@gmail.com>
2023-11-08 18:43:03 -0500

    applied some styles to the aboutpage

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function About() {
   return (
     <>
-      <div>About</div>
-      <p>
+      <p className="fs-2 mt-5 px-5">
         The purpose of the app is for the user to easily navigate to the name of
         the city, view an image of the city, see the population, see the area of
         the city in square miles , the number of annual visitors, the currency
