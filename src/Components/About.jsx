b8f44107335211ea89980b3f440ca28b137598ac
jameel914 <jameelibrahim107@gmail.com>
2023-11-08 20:08:23 -0500

    added styles to the about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function About() {
   return (
     <>
-      <p className="fs-2 mt-5 px-5">
-        The purpose of the app is for the user to easily navigate to the name of
-        the city, view an image of the city, see the population, see the area of
-        the city in square miles , the number of annual visitors, the currency
-        and if the city is the capital.
-      </p>
+      <div className="" style={{ border: "2px solid grey" }}>
+        <p className="fs-2 mt-5 px-5 mb-5">
+          The purpose of the app is for the user to easily navigate to the name
+          of the city, view an image of the city, see the population, see the
+          area of the city in square miles , the number of annual visitors, the
+          currency and if the city is the capital.
+        </p>
+      </div>
     </>
   );
 }
